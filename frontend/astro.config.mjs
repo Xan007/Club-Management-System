63d408dc0bc3e0691cd4e81bb1f2488ec5c6fc29
Xan007 <sierrasander29@gmail.com>
2025-12-08 16:59:25 -0500

    Change vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 import { fileURLToPath } from 'url'
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   vite: {
     resolve: {
       alias: {
